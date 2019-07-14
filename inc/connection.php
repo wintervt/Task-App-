c97d504457a0522de68b424f7e77980d0ac2974d
Milan Toth <mila23ster@gmail.com>
2019-07-14 22:30:13 +0100

    Task App practice project

---
+++ inc/connection.php
@@ -0,0 +1,9 @@
+<?php
+//connecting to DB
+try {
+    $db = new PDO("sqlite:".__DIR__."/database.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    exit;
+}
